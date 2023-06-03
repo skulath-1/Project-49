f754d459caff59685533185f68d6f77f3306c85e
skulath-1 <103314965+skulath-1@users.noreply.github.com>
2023-06-03 11:54:40 +0100

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,32 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+const accessCode4 = "CODING"
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("C U T N I F O N", 600,50)
+    fill("lightblue")
+    text("Hint: Performs a particular task !!", 600,70)
+
+    fill("white")
+    textSize(15)
+    text("A T E D A S B A", 100,250)
+    fill("lightblue")
+    text("Hint: Stores all information !!", 100,270)
+
+    fill("white")
+    textSize(15)
+    text("D C I G O N", 600,250)
+    fill("lightblue")
+    text("Hint: Needed for all websites !!", 600,270)
+
+}
